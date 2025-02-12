2efe38f664b1d23cac126fb4fb980ade6c2cdda8
kubackikacper <kacper1048@gmail.com>
2025-02-12 15:42:03 +0100

    inwork

---
+++ ECommerceApp/DataTransferObjects/ProductDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using ECommerceApp.Models;
+using Microsoft.EntityFrameworkCore;
+namespace ECommerceApp.DataTransferObjects
+{
+    public class ProductDTO
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string ImageURL { get; set; }
+    }
+}
